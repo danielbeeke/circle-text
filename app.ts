54f8e8d722149da13fd2d4c16d7f5c4ada7d7586
Daniel Beeke (NL) <daniel.beeke@om.org>
2021-04-03 21:40:26 +0200

    Playing around with the styling

--- app.ts
+++ app.ts
@@ -1,17 +1,27 @@
-const circleTexts = ([...document.querySelectorAll('.circle-text')] as Array<HTMLDivElement>)
-
-for (const circleText of circleTexts) {
-  circleText.innerHTML = circleText.innerText
-  .split('')
-  .map((letter: string, index: number) => `<span style="--index: ${index}">${letter}</span>`)
-  .join('')
-}
-
 const style = document.body.style
 
 const addDegree = (circle: number, degrees: number) => {
   const previousDegress = parseInt(style.getPropertyValue(`--circle-${circle}-start`))
   style.setProperty(`--circle-${circle}-start`, `${previousDegress + degrees}deg`);  
+}
+
+const circleTexts = ([...document.querySelectorAll('.circle-text')] as Array<HTMLDivElement>)
+
+for (const [circleTextIndex, circleText] of circleTexts.entries()) {
+  circleText.innerHTML = circleText.innerText
+  .split('')
+  .map((letter: string, index: number) => `<span style="--index: ${index}">${letter}</span>`)
+  .join('')
+
+  circleText.addEventListener('wheel', (event: WheelEvent) => {
+    document.body.classList.add('is-scrolling')
+    addDegree(circleTextIndex + 1, event.deltaY)
+    circleText.addEventListener('mouseout', () => {
+      document.body.classList.remove('is-scrolling')
+    }, {
+      once: true
+    })
+  });
 }
 
 const enter = document.querySelector('.enter')
@@ -21,12 +31,13 @@ enter.addEventListener('click', () => {
   addDegree(1, 60)
   setTimeout(() => addDegree(2, 70), 120);
   addDegree(3, 220)
+  setTimeout(() => location.reload(), 3000);
 })
 
 let isRunning = false
 
 enter.addEventListener('mouseenter', () => {
-  if (isRunning) return
+  if (isRunning || document.body.classList.contains('start-transition')) return
   isRunning = true
 
   addDegree(3, 30)
@@ -40,3 +51,8 @@ enter.addEventListener('mouseenter', () => {
     isRunning = false
   }, 1000);
 })
+
+document.body.classList.remove('start')
+setTimeout(() => {
+  document.body.classList.remove('start-transition')
+}, 2000);
